8aaabcbe2aa0ee3ac383b80cf7ab53ae8c9563f2
Michael Schwartz <mykesx@gmail.com>
2014-06-12 12:34:32 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mschwartz
+ * Date: 7/23/13
+ * Time: 11:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/*global decaf, require */
+
+"use strict";
+
+decaf.extend(exports, {
+    Mail: require('lib/Mail').Mail
+});
